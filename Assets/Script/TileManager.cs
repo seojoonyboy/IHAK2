c8a24e15ed57c42616465c9c6f37d72a1e9d04a2
joonyboy <seojoonwon@naver.com>
2018-12-19 17:27:37 +0900

    TileManager.cs에 .editorconfig 적용

--- Assets/Script/TileManager.cs
+++ Assets/Script/TileManager.cs
@@ -9,25 +9,24 @@ public class TileManager : MonoBehaviour {
     public GameObject tile;
     [SerializeField]
     public GameObject tileGroup;
-    
-	void Start () {
+
+    void Start() {
         Grid2D grid = Grid2D.instance;
 
         Vector3 gridLocation = grid.transform.position;
         gridLocation.z = 0;
         GameObject GroupPosition = Instantiate(tileGroup, gridLocation, Quaternion.identity);
 
-        for(int i = 0; i < grid.CellGetIndex(grid.rowCount,grid.columnCount) + 1; i++) 
-        {
+        for (int i = 0; i < grid.CellGetIndex(grid.rowCount, grid.columnCount) + 1; i++) {
             Vector3 tilePosition = grid.CellGetPosition(i);
             tilePosition.z = 0;
             GameObject temp = Instantiate(tile, tilePosition, Quaternion.identity);
-            temp.transform.SetParent(GroupPosition.transform);            
-        }		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+            temp.transform.SetParent(GroupPosition.transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
