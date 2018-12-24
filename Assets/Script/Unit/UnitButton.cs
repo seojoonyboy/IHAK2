692cb30303f118f9c1809ebb59b48f6cc7a43bf3
Kwak Yong Ju <amese1225@fbl.kr>
2018-12-24 18:19:59 +0900

    유닛 편집 추가중

---
+++ Assets/Script/Unit/UnitButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitButton : MonoBehaviour, IDropHandler {
+
+    public GameObject unit {
+        get {
+            if (transform.childCount > 0)
+                return transform.GetChild(0).gameObject;
+
+            return null;
+        }        
+    }
+
+    public void OnDrop (PointerEventData eventData) {
+
+        if(unit == false) {
+
+        }        
+    }	
+}
