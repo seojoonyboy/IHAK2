c73a80c16f3ef5bb41326f89dca8dfba58785eff
joonyboy <seojoonwon@naver.com>
2018-12-26 19:45:49 +0900

    PlayerInfosManager Prefab 및 MenuScene에 추가. 내부에 DummyDeck 추가. DummyDeck을 DeckList로 보여주기 임시 구성 완료. Deck관련 정보(종족, 덱이름)를 담는 별도의 독립 Class 추가. namespace 지정. 따로 Scene에는 추가 안함.

---
+++ Assets/Script/DataModules/Deck.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataModules {
+    [System.Serializable]
+    public class Deck {
+        public string Name;
+        public Species.Type species;
+        public bool isLeader;
+    }
+}
---
+++ Assets/Script/DataModules/Species.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataModules {
+    [System.Serializable]
+    public class Species {
+        public enum Type {
+            OGAH,           //임시명 : 원시 종족
+            FUTURISTIC,     //임시명 : 미래 도시 종족
+        }
+    }
+}
+
