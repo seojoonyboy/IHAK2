914cb4d9eada200067c70262aad820ee31999b46
이종욱 <wjlee729@fbl.kr>
2018-12-27 16:59:45 +0900

    Modal(확인) 창 구현

--- Assets/Script/Modal.cs
+++ Assets/Script/Modal.cs
@@ -9,18 +9,34 @@ public class Modal : MonoBehaviour {
 	[SerializeField] private Button noButton;
 	[SerializeField] private Text describe;
 
-	public static void instantiate(string text, UnityAction function) {
+	public enum Type {
+		YESNO,
+		CHECK,
+		INSERT
+	}
+	/// <summary>
+	/// Modal 창 생성기
+	/// </summary>
+	/// <param name="text">설명에 들어갈 내용</param>
+	/// <param name="type">Modal.Type.종류</param>
+	/// <param name="function">yes 버튼 누를 경우 실행 함수(필요하면)</param>
+	public static void instantiate(string text, Type type, UnityAction function = null) {
 		GameObject modal = Resources.Load("Prefabs/ModalWindow", typeof(GameObject)) as GameObject;
         Canvas canvas = (Canvas)FindObjectOfType(typeof(Canvas));
         if(canvas == null) {
             Debug.LogError("no Canvas");
             return;
         }
-        Instantiate(modal, canvas.transform, false).GetComponent<Modal>().setData(text, function);
+        Instantiate(modal, canvas.transform, false).GetComponent<Modal>().setData(text, function, type);
 	}
 
-	public void setData(string text, UnityAction function) {
+	public void setData(string text, UnityAction function, Type type) {
+		if(type == Type.CHECK) {
+			noButton.gameObject.SetActive(false);
+			yesButton.GetComponentInChildren<Text>().text = "확인";
+		}
 		describe.text = text;
+		if(function == null) return;
 		yesButton.onClick.AddListener(function);
 	}
 
