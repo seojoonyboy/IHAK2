f74edc25007449e142c0df715927a8056be58f8b
joonyboy <seojoonwon@naver.com>
2019-01-04 16:43:16 +0900

    종족 선택 모달 추가.

--- Assets/Script/Controllers/DeckSettingController.cs
+++ Assets/Script/Controllers/DeckSettingController.cs
@@ -1,20 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DataModules;
 using System;
+using UniRx;
+using UniRx.Triggers;
 
 public class DeckSettingController : MonoBehaviour {
     GameSceneManager.SceneState sceneState = GameSceneManager.SceneState.DeckSettingScene;
     GameSceneManager gsm;
     PlayerInfosManager playerInfosManager;
-    [SerializeField]
-    public GameObject deckSet;
+
+    [SerializeField] public GameObject deckSet;
+    [SerializeField] private GameObject togglePref;
+    [SerializeField] private Button chooseSpeciesBtn;
+    [SerializeField] private Button speciesConfirmBtn;
+    [SerializeField] private GameObject modal;
+    [SerializeField] Sprite[] speciesPortraits;
+
+    public Text 
+        modalHeader,
+        content;
+
+    private int speciesId = 0;
+    public int SpeciesId {
+        get {
+            return speciesId;
+        }
+    }
 
     private void Start() {
         playerInfosManager = PlayerInfosManager.Instance;
         gsm = FindObjectOfType<GameSceneManager>();
         deckSet = GameObject.FindGameObjectWithTag("TileGroup");
+
+        chooseSpeciesBtn.onClick
+            .AsObservable()
+            .Subscribe(_ => {
+                modal.SetActive(true);
+            });
+        speciesConfirmBtn.onClick
+            .AsObservable()
+            .Subscribe(_ => {
+                modal.SetActive(false);
+            });
+
+        InitToggles();
+    }
+
+    private void InitToggles() {
+        int length = Enum.GetValues(typeof(Species.Type)).Length;
+        for(int i=0; i<length; i++) {
+            ToggleGroup toggleGroup = modal.transform.Find("InnerModal/Body/DataArea/ToggleGroup").GetComponent<ToggleGroup>();
+            GameObject pref = Instantiate(togglePref, toggleGroup.transform);
+            pref.name = "Toggle" + i;
+            int id = i;
+            Toggle toggle = pref.GetComponent<Toggle>();
+            toggle.group = toggleGroup;
+            if(i == 0) {
+                toggle.isOn = true;
+                Toggle(toggle, id);
+            }
+            toggle.onValueChanged.AddListener(delegate { Toggle(toggle, id); });
+            Image image = pref.transform.Find("Background/Image").GetComponent<Image>();
+            image.sprite = speciesPortraits[i];
+        }
+    }
+
+    private void Toggle(Toggle t, int id) {
+        if (t.isOn) {
+            SetModalText(id);
+        }
+    }
+
+    private void SetModalText(int id) {
+        string species = ((Species.Type)id).ToString();
+        modalHeader.text = species;
+        content.text = species + "에 대한 설명";
     }
 
     public void settingButton() {
@@ -25,6 +88,7 @@ public class DeckSettingController : MonoBehaviour {
         Deck deck = new Deck();
         deck.Id = playerInfosManager.decks.Capacity;
         deck.deckData = transform.GetChild(0).GetChild(0).GetComponent<DropHandler>().deckData; // 타일 하위 오브젝트 스크립트 말고 그룹용 오브젝트 스크립트를 만들어야하나?
+        deck.species = (Species.Type)speciesId;
         deck.Name = inputText;
 
         playerInfosManager.AddDeck(deck);
