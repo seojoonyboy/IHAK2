80d5f09034decb66310ede847121001b0e9b819f
KHG <cow3323@fbl.kr>
2019-01-08 18:30:05 +0900

    인게임 기본 UI 구현

---
+++ Assets/Script/Controllers/IngameSceneUIController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngameSceneUIController : MonoBehaviour {
+
+    private GameSceneManager.SceneState sceneState = GameSceneManager.SceneState.IngameScene;
+
+    [SerializeField] Transform territoryList;
+    [SerializeField] Transform camera;
+
+    private float mousDownPosition;
+
+	// Use this for initialization
+	void Start () {
+
+        var downStream = territoryList.gameObject.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0)).Select(_ => mousDownPosition = Input.mousePosition.x);
+        var upStream = territoryList.gameObject.UpdateAsObservable().Where(_ => Input.GetMouseButtonUp(0)).Select(_ => Input.mousePosition.x);
+        var dragStream = territoryList.gameObject.UpdateAsObservable().SkipUntil(downStream).TakeUntil(upStream).RepeatUntilDestroy(this);
+        dragStream.Where(_ => mousDownPosition - Input.mousePosition.x < -500).ThrottleFirst(TimeSpan.FromMilliseconds(450)).Subscribe(_ => SwitchTerritory(false));
+        dragStream.Where(_ => mousDownPosition - Input.mousePosition.x > 500).ThrottleFirst(TimeSpan.FromMilliseconds(450)).Subscribe(_ => SwitchTerritory(true));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void SwitchTerritory(bool left) {
+        if (left) {
+            iTween.MoveTo(territoryList.gameObject, iTween.Hash("x", territoryList.transform.position.x + Screen.width, "time", 0.4f, "delay", 0, "easetype", iTween.EaseType.easeInOutQuart));
+            
+        }
+        else {
+            iTween.MoveTo(territoryList.gameObject, iTween.Hash("x", territoryList.transform.position.x - Screen.width, "time", 0.4f, "delay", 0, "easetype", iTween.EaseType.easeInOutQuart));
+        }
+    }
+}
