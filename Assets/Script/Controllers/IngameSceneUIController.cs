0d1feea70d0fa4d24d558194520391b5d6785b9a
KHG <cow3323@fbl.kr>
2019-01-09 13:55:23 +0900

    ScriptMissing 해결, IngameScene UI 작업중

--- Assets/Script/Controllers/IngameSceneUIController.cs
+++ Assets/Script/Controllers/IngameSceneUIController.cs
@@ -17,12 +17,11 @@ public class IngameSceneUIController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
         var downStream = territoryList.gameObject.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0)).Select(_ => mousDownPosition = Input.mousePosition.x);
         var upStream = territoryList.gameObject.UpdateAsObservable().Where(_ => Input.GetMouseButtonUp(0)).Select(_ => Input.mousePosition.x);
         var dragStream = territoryList.gameObject.UpdateAsObservable().SkipUntil(downStream).TakeUntil(upStream).RepeatUntilDestroy(this);
-        dragStream.Where(_ => mousDownPosition - Input.mousePosition.x < -500).ThrottleFirst(TimeSpan.FromMilliseconds(450)).Subscribe(_ => SwitchTerritory(false));
-        dragStream.Where(_ => mousDownPosition - Input.mousePosition.x > 500).ThrottleFirst(TimeSpan.FromMilliseconds(450)).Subscribe(_ => SwitchTerritory(true));
+        dragStream.Where(_ => mousDownPosition - Input.mousePosition.x < -500).ThrottleFirst(TimeSpan.FromMilliseconds(450)).Subscribe(_ => StartCoroutine(SwitchTerritory(true)));
+        dragStream.Where(_ => mousDownPosition - Input.mousePosition.x > 500).ThrottleFirst(TimeSpan.FromMilliseconds(450)).Subscribe(_ => StartCoroutine(SwitchTerritory(false)));
     }
 	
 	// Update is called once per frame
@@ -30,13 +29,19 @@ public class IngameSceneUIController : MonoBehaviour {
 		
 	}
 
-    private void SwitchTerritory(bool left) {
-        if (left) {
-            iTween.MoveTo(camera.gameObject, iTween.Hash("x", camera.transform.position.x + Screen.width, "time", 0.4f, "delay", 0, "easetype", iTween.EaseType.easeInOutQuart));
-            
-        }
-        else {
+    IEnumerator SwitchTerritory(bool left) {
+        if (left && camera.transform.position.x > 1079) {
             iTween.MoveTo(camera.gameObject, iTween.Hash("x", camera.transform.position.x - Screen.width, "time", 0.4f, "delay", 0, "easetype", iTween.EaseType.easeInOutQuart));
+            yield return new WaitForSeconds(0.38f);
+            this.transform.GetChild(1).gameObject.SetActive(false);
+            this.transform.GetChild(0).gameObject.SetActive(true);
         }
+        else if (!left && camera.transform.position.x < 1) {
+            iTween.MoveTo(camera.gameObject, iTween.Hash("x", camera.transform.position.x + Screen.width, "time", 0.4f, "delay", 0, "easetype", iTween.EaseType.easeInOutQuart));
+            yield return new WaitForSeconds(0.38f);
+            this.transform.GetChild(0).gameObject.SetActive(false);
+            this.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        
     }
 }
