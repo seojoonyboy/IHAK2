ae8dfe53dc63d7ab7999c0ab69eae5b160627d8e
joonyboy <seojoonwon@naver.com>
2019-01-17 22:24:30 +0900

    유닛 생산건물 모달 기능 추가. 한글이 깨지는 문제가 있어서 해결중

--- Assets/Script/UI/OwnCardGenerator.cs
+++ Assets/Script/UI/OwnCardGenerator.cs
@@ -9,7 +9,8 @@ public class OwnCardGenerator : MonoBehaviour {
     public GameObject
         pageObject,
         slotObject,
-        detailModal;
+        prodDetailModal,
+        unitGenDetailModal;
 
     [Header("UI")]
     public Sprite[] cardPanels;
@@ -50,16 +51,55 @@ public class OwnCardGenerator : MonoBehaviour {
     }
 
     private void ShowDetail(BuildingObject buildingObject) {
-        detailModal.SetActive(true);
-        Transform innerModal = detailModal.transform.GetChild(0);
+        if(buildingObject.data.card.unit == null) {
+            prodDetailModal.SetActive(true);
+            Transform innerModal = prodDetailModal.transform.GetChild(0);
 
-        Text hp = innerModal.Find("ImageArea/Image/HPArea/Value").GetComponent<Text>();
-        Text header = innerModal.Find("Header/Text").GetComponent<Text>();
-        Text limitCount = innerModal.Find("DataArea/LimitCount/Text").GetComponent<Text>();
+            Text hp = innerModal.Find("DataArea/UpperBody/HP/Value").GetComponent<Text>();
+            Text header = innerModal.Find("Header/Text").GetComponent<Text>();
+            Text limitCount = innerModal.Find("Upper/LimitCount/Value").GetComponent<Text>();
+            Text tier = innerModal.Find("Upper/DataArea/Header/TierName").GetComponent<Text>();
 
-        Card card = buildingObject.data.card;
-        hp.text = card.hitPoint.ToString();
-        header.text = card.name;
-        limitCount.text = card.placementLimit.ToString();
+            Text food = innerModal.Find("DataArea/UpperBody/Food/Value").GetComponent<Text>();
+            Text env = innerModal.Find("DataArea/UpperBody/Env/Value").GetComponent<Text>();
+            Text gold = innerModal.Find("DataArea/UpperBody/Gold/Value").GetComponent<Text>();
+
+            Card card = buildingObject.data.card;
+            hp.text = card.hitPoint.ToString();
+            header.text = card.name;
+            limitCount.text = "한도 " + card.placementLimit.ToString();
+
+            tier.text = card.rareity + " 등급";
+            food.text = card.product.food.ToString();
+            gold.text = card.product.gold.ToString();
+        }
+        else {
+            unitGenDetailModal.SetActive(true);
+            Transform innerModal = unitGenDetailModal.transform.GetChild(0);
+
+            Text tier = innerModal.Find("Upper/DataArea/Header/TierName").GetComponent<Text>();
+            Text header = innerModal.Find("Header/Text").GetComponent<Text>();
+
+            Text unitName = innerModal.Find("DataArea/UpperBody/Text").GetComponent<Text>();
+            Text needResources = innerModal.Find("DataArea/UpperBody/NeedResource").GetComponent<Text>();
+            Text unitSpec = innerModal.Find("DataArea/BottomBody/UnitSpec").GetComponent<Text>();
+
+            Card card = buildingObject.data.card;
+            Unit unit = card.unit;
+            tier.text = unit.tearNeed + " 등급";
+            header.text = card.name;
+
+            unitName.text = "유닛생산 " + unit.name;
+            needResources.text = "식량 : " + unit.cost.food + "\n"
+                + "골드 : " + unit.cost.gold + "\n"
+                + "환경 : " + unit.cost.environment + "\n";
+
+            unitSpec.text = "체력 : " + unit.hitPoint
+                + "공격력 : " + unit.power
+                + "공격 속도 : " + unit.attackSpeed
+                + "공격 범위 : " + unit.attackRange
+                + "이동 속도 : " + unit.moveSpeed
+                + "요구 레벨 : " + unit.tearNeed;
+        }
     }
 }
