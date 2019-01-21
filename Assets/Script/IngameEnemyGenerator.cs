1d78fd22bc013f23c7e7d784e8d9492a62eacb66
Kwak Yong Ju <amese1225@fbl.kr>
2019-01-21 20:41:18 +0900

    IngameScene안에 UICanvas -> Horizontal Scroll Snap -> Content -> Territory안에 EnemyCity 추가, EnemyCity안에 IngameEnemyGenerator추가, EnemyTileGroup안에 미리 저장해둔 적 도시 tileGroup 프리팹 추가

---
+++ Assets/Script/IngameEnemyGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameEnemyGenerator : MonoBehaviour {
+
+    public GameObject enemyTileGroup;
+    /*
+    public List<int> enemyBuildingList =  new List<int>{ 28, 36, 36, 49 ,47,
+                                                         38, 44, 672, 29, 32,
+                                                         40, 48, -1, 29, 32,
+                                                         40, 48, 48, 30, 32,
+                                                         47, 35, 35, 35, 28 };
+                                                         */
+
+    private void Start() {
+        Instantiate(enemyTileGroup, transform);
+    }
+
+}
