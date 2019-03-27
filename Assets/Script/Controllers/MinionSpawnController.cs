dc25b31f029ef6280be3c11d0f40caa2729440f7
KHG <cow3323@fbl.kr>
2019-03-27 19:06:25 +0900

    미니언 생산 Script 작업중

--- Assets/Script/Controllers/MinionSpawnController.cs
+++ Assets/Script/Controllers/MinionSpawnController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity;
 using Sirenix.OdinInspector;
 
 
@@ -13,30 +14,17 @@ public partial class MinionSpawnController : SerializedMonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
 
 public partial class MinionSpawnController : SerializedMonoBehaviour {
+    [Header(" - Player Identity")]
+    [SerializeField] PlayerController.Player playerNum;
+
     [Header(" - Prefabs")]
-    [DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.ExpandedFoldout)]
-    public Dictionary<string, GameObject> minionPrefabs;
+    [SerializeField] GameObject shortDisMinion;
+    [SerializeField] GameObject longDisMinion;
 
-    [SerializeField] Transform summonParent;
 
-    //public void HeroSummon(ActiveCard card) {
-    //    var result = GetHeroPrefab(card.baseSpec.unit.id);
-    //    if (result == null) {
-    //        Debug.LogError("해당 유닛의 프리팹을 찾을 수 없습니다!");
-    //        return;
-    //    }
-    //    GameObject hero = Instantiate(result, summonParent);
-    //    UnitAI unitAI = hero.GetComponent<UnitAI>();
-    //    unitAI.SetUnitData(card);
-    //}
-
-    //public GameObject GetHeroPrefab(string id) {
-    //    if (!heroPrefabs.ContainsKey(id)) return null;
-    //    return heroPrefabs[id];
-    //}
 }
