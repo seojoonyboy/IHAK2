1e490464008ee3a56f0e45f44f28d01f62304ef9
joonyboy <seojoonwon@naver.com>
2019-03-27 16:16:44 +0900

    decimal 형의 카드 비용이 인게임에서 0으로 표기되는 오류 수정. 덱 셔플 시스템 수정.

--- Assets/Script/BuildingObject.cs
+++ Assets/Script/BuildingObject.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using DataModules;
 using Spine.Unity;
 public class BuildingObject : MonoBehaviour {
-    [SerializeField] public Card card;
+    public Card card;
     public Sprite icon;
     public Sprite mainSprite;
     public Sprite upgradeIcon;
