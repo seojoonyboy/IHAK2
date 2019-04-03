0be007cced69d46cc624a54035bb11633fda29d7
joonyboy <seojoonwon@naver.com>
2019-04-03 19:41:56 +0900

    전쟁의 함성, 무서운 예언 Buff 클래스를 상속 받도록 수정. 관련 인자값 할당.

---
+++ Assets/Script/Ingame/Skills/Buff.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Buff : MonoBehaviour {
+    public int moveSpeed_percentage;
+    public int hp_percentage;
+    public float attackSpeed;
+    public int power;
+    public int maxHp;
+}
--- Assets/Script/Ingame/Skills/ScaryOracle.cs
+++ Assets/Script/Ingame/Skills/ScaryOracle.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class ScaryOracle : MonoBehaviour {
+public class ScaryOracle : Buff {
     public int percentage;
 
     void Start() {
-        GetComponent<UnitAI>().ChangeSpeedByPercentage(-percentage);
+        moveSpeed_percentage = percentage;
+        //GetComponent<UnitAI>().ChangeSpeedByPercentage(-percentage);
     }
 
     void OnDestroy() {
-        GetComponent<UnitAI>().ResetSpeedPercentage();
+        //GetComponent<UnitAI>().ResetSpeedPercentage();
     }
 }
