ee98939f78573d82858dda2c8fb3f7e4fffdb771
joonyboy <seojoonwon@naver.com>
2019-04-09 17:39:10 +0900

    development url 복구

--- Assets/Script/Main/Managers/NetworkManager.cs
+++ Assets/Script/Main/Managers/NetworkManager.cs
@@ -5,8 +5,7 @@ using UnityEngine.Networking;
 public class NetworkManager : Singleton<NetworkManager> {
 #if DEVELOPMENT_BUILD
     //개발용
-    //public string baseUrl = "http://ihak2devclient.fbl.kr/";
-    public string baseUrl = "http://ihak2test.fbl.kr/";
+    public string baseUrl = "http://ihak2devclient.fbl.kr/";
 #else
     //릴리즈용
     public string baseUrl = "http://ihak2test.fbl.kr/";
