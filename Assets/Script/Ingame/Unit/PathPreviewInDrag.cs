eefa121236ce79e2849098a3cd02bebd0d8aa819
joonyboy <seojoonwon@naver.com>
2019-04-11 13:53:27 +0900

    Node 레이어 지정. 영웅 카드 드래그 할때만 BoxCast를 발생시키도록 다시 수정. Collider가 멀리 떨어져 있어도 Ray에 인식되는 문제 수정. 기즈모 초록색 색상 지정

--- Assets/Script/Ingame/Unit/PathPreviewInDrag.cs
+++ Assets/Script/Ingame/Unit/PathPreviewInDrag.cs
@@ -19,22 +19,12 @@ namespace IngameModule {
         void Awake() {
             m_Raycaster = GetComponentInParent<GraphicRaycaster>();
             m_PointEventData = new PointerEventData(FindObjectOfType<EventSystem>());
-            mask = LayerMask.GetMask("Default");
+            mask = LayerMask.GetMask("Node");
         }
 
         void Start() {
             coroutine = Serach();
             StartCoroutine(coroutine);
-        }
-
-        void Update() {
-            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.BoxCast(point, new Vector2(10, 10), 0, new Vector3(point.x, point.y, -point.z * 100), Mathf.Infinity, layerMask : mask);
-            //RaycastHit2D hit = Physics2D.Raycast(point, new Vector3(point.x, point.y, -point.z * 100), Mathf.Infinity);
-            Debug.DrawLine(point, new Vector3(point.x, point.y, -point.z * 100), Color.red);
-            if (hit.collider != null) {
-                Debug.Log(hit.transform.gameObject.name);
-            }
         }
 
         public void OnDrag(PointerEventData eventData) {
@@ -45,18 +35,11 @@ namespace IngameModule {
             while (true) {
                 if (canSearch) {
                     Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    RaycastHit2D hit = Physics2D.BoxCast(point, new Vector2(10, 10), 0, new Vector3(point.x, point.y, -point.z * 100), Mathf.Infinity);
-                    //RaycastHit2D hit = Physics2D.Raycast(point, new Vector3(point.x, point.y, -point.z * 100), Mathf.Infinity);
+                    RaycastHit2D hit = Physics2D.BoxCast(point, new Vector2(10, 10), 0, Vector3.forward, Mathf.Infinity, layerMask: mask);
                     Debug.DrawLine(point, new Vector3(point.x, point.y, -point.z * 100), Color.red);
                     if (hit.collider != null) {
                         Debug.Log(hit.transform.gameObject.name);
                     }
-                    //m_PointEventData.position = Input.mousePosition;
-                    //List<RaycastResult> results = new List<RaycastResult>();
-                    //m_Raycaster.Raycast(m_PointEventData, results);
-                    //foreach (var result in results) {
-                    //    Debug.Log(result.ToString());
-                    //}
                 }
                 yield return new WaitForSeconds(1.0f);
             }
@@ -74,6 +57,7 @@ namespace IngameModule {
 
         void OnDrawGizmos() {
             Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Gizmos.color = Color.green;
             Gizmos.DrawWireCube(point, new Vector2(10, 10));
         }
     }
