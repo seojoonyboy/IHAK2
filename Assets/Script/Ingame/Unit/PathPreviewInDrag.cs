f5cd22df1da8b2f77fb89def1c0a32671d693c5d
joonyboy <seojoonwon@naver.com>
2019-04-11 14:28:45 +0900

    영웅 카드 드래그시 MapNode의 길찾기와 1차 연동.

--- Assets/Script/Ingame/Unit/PathPreviewInDrag.cs
+++ Assets/Script/Ingame/Unit/PathPreviewInDrag.cs
@@ -15,7 +15,7 @@ namespace IngameModule {
         private bool canSearch = false;
 
         LayerMask mask;
-
+        MapStation hq_mapStation;
         void Awake() {
             m_Raycaster = GetComponentInParent<GraphicRaycaster>();
             m_PointEventData = new PointerEventData(FindObjectOfType<EventSystem>());
@@ -25,6 +25,8 @@ namespace IngameModule {
         void Start() {
             coroutine = Serach();
             StartCoroutine(coroutine);
+
+            hq_mapStation = PlayerController.Instance.hq_mapStation;
         }
 
         public void OnDrag(PointerEventData eventData) {
@@ -38,7 +40,11 @@ namespace IngameModule {
                     RaycastHit2D hit = Physics2D.BoxCast(point, new Vector2(10, 10), 0, Vector3.forward, Mathf.Infinity, layerMask: mask);
                     Debug.DrawLine(point, new Vector3(point.x, point.y, -point.z * 100), Color.red);
                     if (hit.collider != null) {
-                        Debug.Log(hit.transform.gameObject.name);
+                        //Debug.Log(hit.transform.gameObject.name);
+                        List<Vector3> path = PathFind(hit.transform.gameObject);
+                        foreach(Vector3 val in path) {
+                            Debug.Log(val);
+                        }
                     }
                 }
                 yield return new WaitForSeconds(1.0f);
@@ -59,6 +65,29 @@ namespace IngameModule {
             Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Gizmos.color = Color.green;
             Gizmos.DrawWireCube(point, new Vector2(10, 10));
+        }
+
+        List<Vector3> PathFind(GameObject collision) {
+            MapStation station = collision.GetComponent<MapStation>();
+            List<Vector3> path = new List<Vector3>();
+            if(station != null) {
+                path = MapNode.SearchPosition(
+                    hq_mapStation,
+                    station.mapPostion
+                );
+                return path;
+            }
+
+            MapRoad mapRoad = collision.GetComponent<MapRoad>();
+            if(mapRoad != null) {
+                path = MapNode.SearchPosition(
+                    hq_mapStation, 
+                    mapRoad.mapPostion
+                );
+                return path;
+            }
+
+            return path;
         }
     }
 }
