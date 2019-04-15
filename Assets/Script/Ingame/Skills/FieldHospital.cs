323c2c58d096229f18840a037a5d6d40df1e3717
Kwak Yong Ju <amese1225@fbl.kr>
2019-04-15 17:57:32 +0900

    치료소 범위 콜라이더 내에 진입시에 Layer 10일 경우에 300까지 치료가 되도록 추가

--- Assets/Script/Ingame/Skills/FieldHospital.cs
+++ Assets/Script/Ingame/Skills/FieldHospital.cs
@@ -26,19 +26,15 @@ public class FieldHospital : IngameBuilding {
 
     private void healUnit(GameObject target) {
         if (target.layer != 10) return;
-        HeroAI hero = target.GetComponent<HeroAI>();
         UnitAI unit = target.GetComponent<UnitAI>();
 
-        if (hero && unit == null) return;
+        if (unit == null) return;
 
-        if(hero != null) {
-            if (hero.health < 300)
-                hero.health += 10;
-        }
-        else if(unit != null) {
-            if (unit.health < 300)
-                unit.health += 10;
-        }       
+
+
+        if (unit.health < 300)
+            unit.health += 10;
+             
     }
 
 
