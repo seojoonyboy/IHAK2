3e050a360ebce616f100e9c0f05b7846c09f1149
Kwak Yong Ju <amese1225@fbl.kr>
2019-04-16 17:12:06 +0900

    전진기지 기초코딩

--- Assets/Script/Ingame/Maps/Station/BaseCampStation.cs
+++ Assets/Script/Ingame/Maps/Station/BaseCampStation.cs
@@ -6,14 +6,52 @@ public partial class BaseCampStation : DefaultStation {
 
     List<GameObject> creepList;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    public List<GameObject> targets;
+
+    // Use this for initialization
+    void Start () {
         OwnerNum = PlayerController.Player.NEUTRAL;
         creepList = new List<GameObject>();
+        targets = new List<GameObject>();
+        Building = Resources.Load("Prefabs/FowardHQ") as GameObject;
+        Instantiate(Building, transform);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    IEnumerator FindOwner() {
+        bool find = true;
+        int targetLayer = 0;
+        while (find) {
+            foreach (GameObject target in targets) {
+                if (target == null) continue;
+                if ((int)OwnerNum != target.layer) {
+                    int tempLayer = targetLayer;
+                    targetLayer = target.layer;
+                    if (tempLayer != targetLayer)
+                        find = false;
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        OwnerNum = (PlayerController.Player)targetLayer;
+    }
+
+
+    void OnTriggerStay2D(Collider2D collision) {
+        if ((collision.gameObject.layer != (int)OwnerNum) && collision.GetComponent<UnitAI>() != null && targets.Contains(collision.gameObject) == false) {
+            if (!targets.Exists(x => x == collision.gameObject)) targets.Add(collision.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if ((collision.gameObject.layer != (int)OwnerNum) && collision.GetComponent<UnitAI>() != null) {
+            targets.Remove(collision.gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 		if(creepList.Count == 0) {
 
         }
