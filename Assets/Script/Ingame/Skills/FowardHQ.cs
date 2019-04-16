f9748576c026e818ad535d504a28314e7cc7e9f6
Kwak Yong Ju <amese1225@fbl.kr>
2019-04-16 10:46:39 +0900

    체력 회복 연산추가, 전진기지 작업중

---
+++ Assets/Script/Ingame/Skills/FowardHQ.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FowardHQ : IngameBuilding {
+    public CircleCollider2D effectRange;
+    // Use this for initialization
+    void Start() {
+        maxHp = 300;
+        buildingHp = maxHp;
+        effectRange = transform.gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    private void setRange(float amount) {
+        effectRange.radius = amount;
+    }
+
+}
