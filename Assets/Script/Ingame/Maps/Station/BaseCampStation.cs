fb4d8143e89fded765209b7c7239c28b1a691b47
KHG <cow3323@fbl.kr>
2019-04-17 15:30:47 +0900

    노드 탐색 제외

--- Assets/Script/Ingame/Maps/Station/BaseCampStation.cs
+++ Assets/Script/Ingame/Maps/Station/BaseCampStation.cs
@@ -38,12 +38,14 @@ public partial class BaseCampStation : DefaultStation {
 
 
     void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.layer != 16) return;
         if ((collision.gameObject.layer != (int)OwnerNum) && collision.GetComponent<UnitAI>() != null && targets.Contains(collision.gameObject) == false) {
             if (!targets.Exists(x => x == collision.gameObject)) targets.Add(collision.gameObject);
         }
     }
 
     void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.layer != 16) return;
         if ((collision.gameObject.layer != (int)OwnerNum) && collision.GetComponent<UnitAI>() != null) {
             targets.Remove(collision.gameObject);
         }
