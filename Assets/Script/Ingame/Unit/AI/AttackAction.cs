f021ee1f73e9152c100ccc67997e33aeb0b4fc93
joonyboy <seojoonwon@naver.com>
2019-04-17 19:37:13 +0900

    디버그 로그 제거

--- Assets/Script/Ingame/Unit/AI/AttackAction.cs
+++ Assets/Script/Ingame/Unit/AI/AttackAction.cs
@@ -19,7 +19,7 @@ public class AttackAction : Action {
             if (target.GetComponent<UnitAI>() != null) {
                 controller.GetComponent<MonsterAI>().monsterSpine.Attack();
             }
-            Debug.Log("공격!");
+            //Debug.Log("공격!");
             controller.GetComponent<Timer>().time = controller.time;
         }
     }
