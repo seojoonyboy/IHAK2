e6ab72d745aa757b008a9fab3d04f24927156583
joonyboy <seojoonwon@naver.com>
2019-04-17 17:20:20 +0900

    중립몬스터 정보 서버에서 받아서 AccountManger에 List 형태로 추가

---
+++ Assets/Script/DataModules/NeutralMonsterData.cs
@@ -0,0 +1,14 @@
+namespace DataModules{
+    [System.Serializable]
+    public class NeutralMonsterData {
+        public string id;
+        public int rarity;
+        public int size;
+        public int attackPower;
+        public float attackSpeed;
+        public int defence;
+        public int attackRange;
+        public int hitPoint;
+        public float moveSpeed;
+    }
+}
