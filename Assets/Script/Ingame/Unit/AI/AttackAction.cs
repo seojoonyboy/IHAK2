5e4f7a3080fe3b933fcd5d88112ecf1a2d8a71f2
joonyboy <seojoonwon@naver.com>
2019-04-17 16:53:14 +0900

    일정 주기마다 공격 Animation 호출하도록 수정. 불필요 코드 제거(주석 포함).

--- Assets/Script/Ingame/Unit/AI/AttackAction.cs
+++ Assets/Script/Ingame/Unit/AI/AttackAction.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "PluggableAI/Actions/Attack")]
 public class AttackAction : Action {
+    private float time;
+    private float interval = 5;
     public override void Act(StateController controller) {
         Attack(controller);
     }
 
     private void Attack(StateController controller) {
-        controller.GetComponent<MonsterAI>().monsterSpine.Attack();
+        var target = controller.chaseTarget;
+        if(time == 0) {
+            time = controller.time;
+        }
+        
+        if (controller.time - time > interval) {
+            if (target.GetComponent<UnitAI>() != null) {
+                controller.GetComponent<MonsterAI>().monsterSpine.Attack();
+            }
+            Debug.Log("공격!");
+            time = 0;
+        }
     }
 }
