4c3b19134e5069de4730a29df4a3a218ec2b060f
이종욱 <wjlee729@fbl.kr>
2019-04-18 17:19:35 +0900

    미니언 & 라칸 spine 데이터 유니티 옮기기

--- Assets/Script/Ingame/Unit/UnitAI.cs
+++ Assets/Script/Ingame/Unit/UnitAI.cs
@@ -50,6 +50,7 @@ public partial class UnitAI : MonoBehaviour {
     }
 
     void Start() {
+        setState(aiState.NONE);
         eventHandler.AddListener(IngameSceneEventHandler.EVENT_TYPE.ORDER_UNIT_RETURN, ReturnDeck);
     }
 
