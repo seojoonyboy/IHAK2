264f878f2c7b9cc7122c6799c5a6292cdbe36d7f
Kwak Yong Ju <amese1225@fbl.kr>
2019-04-23 16:58:48 +0900

    메인화면 미션 리스트 작업중

--- Assets/Script/Main/UI/MissionList.cs
+++ Assets/Script/Main/UI/MissionList.cs
@@ -7,36 +7,40 @@ public class MissionList : MonoBehaviour {
 
     public Sprite unclearSprite;
     public Sprite clearSprite;
-    
+
+    public Transform missionPivot;
     public Transform missionBtnList;
+    public Slider missionSlider;
     public int selectMissionNum = -1;
     public AccountManager accountManager;
 	// Use this for initialization
 	void Start () {
         accountManager = AccountManager.Instance;
         missionBtnList = transform.Find("MissionList");
-        gameObject.SetActive(false);
+        missionPivot = transform.Find("location");
+        missionSlider = transform.Find("missionSlider").GetComponent<Slider>();
         CheckClear();
     }
 
     public void CheckClear() {
-        for(int i = 0; i< accountManager.missionClear; i++) 
-            missionBtnList.GetChild(i).GetComponent<Image>().sprite = clearSprite;        
+        for (int i = 0; i < accountManager.missionClear + 1; i++) {
+            missionBtnList.GetChild(i).GetComponent<Image>().sprite = clearSprite;
+            missionPivot.position = new Vector3(missionBtnList.GetChild(i).position.x, 50);
+            if (i == missionBtnList.childCount - 1) break;
+        }
+        missionSlider.value = (float)accountManager.missionClear / (missionBtnList.childCount - 1);
     }
 	
     
 
     public void SelectMission(int num) {
-        if (accountManager.selectNumber < num) return;
-        selectMissionNum = num;       
+        if (accountManager.missionClear < num) return;
+        selectMissionNum = num;
+        missionPivot.position = new Vector3(missionBtnList.GetChild(num).position.x, 50);
     }
 
     public void StartMission() {
         if (selectMissionNum < accountManager.missionClear) return;
-    }
-
-    public void CloseWindow() {
-        gameObject.SetActive(false);
     }
 
 }
