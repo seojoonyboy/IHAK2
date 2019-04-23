53453e60b41810fa11b1b33375cbaf90fd7b1a4e
Kwak Yong Ju <amese1225@fbl.kr>
2019-04-23 17:58:35 +0900

    메인메뉴씬 미션선택창 작업(현재는 미팝업)

--- Assets/Script/Main/UI/MissionList.cs
+++ Assets/Script/Main/UI/MissionList.cs
@@ -11,14 +11,18 @@ public class MissionList : MonoBehaviour {
     public Transform missionPivot;
     public Transform missionBtnList;
     public Slider missionSlider;
+    public Text missionText;
+
     public int selectMissionNum = -1;
     public AccountManager accountManager;
+    public GameObject startButton;
 	// Use this for initialization
 	void Start () {
         accountManager = AccountManager.Instance;
         missionBtnList = transform.Find("MissionList");
         missionPivot = transform.Find("location");
         missionSlider = transform.Find("missionSlider").GetComponent<Slider>();
+        missionText = transform.Find("missionText").GetComponent<Text>();
         CheckClear();
     }
 
@@ -29,14 +33,41 @@ public class MissionList : MonoBehaviour {
             if (i == missionBtnList.childCount - 1) break;
         }
         missionSlider.value = (float)accountManager.missionClear / (missionBtnList.childCount - 1);
+        SelectMission(accountManager.missionClear);
     }
 	
     
 
     public void SelectMission(int num) {
-        if (accountManager.missionClear < num) return;
         selectMissionNum = num;
-        missionPivot.position = new Vector3(missionBtnList.GetChild(num).position.x, 50);
+        switch (selectMissionNum) {
+            case 0:
+                missionPivot.position = new Vector3(missionBtnList.GetChild(selectMissionNum).position.x, 50);
+                missionText.text = "미션 1 : 이동, 점령, 마법";
+                break;
+            case 1:
+                missionPivot.position = new Vector3(missionBtnList.GetChild(selectMissionNum).position.x, 50);
+                missionText.text = "미션 2 : 거점종류, 레벨업, 후퇴";
+                break;
+            case 2:
+                missionPivot.position = new Vector3(missionBtnList.GetChild(selectMissionNum).position.x, 50);
+                missionText.text = "미션 3 : 더미도시와 겨루기";
+                break;
+            default:
+                missionPivot.position = new Vector3(missionBtnList.GetChild(2).position.x, 50);
+                missionText.text = "미션 3 : 더미도시와 겨루기";
+                break;
+        }
+
+        if (accountManager.missionClear < num) {
+            startButton.GetComponent<Image>().color = Color.gray;
+            startButton.transform.Find("Lock").gameObject.SetActive(true);
+        }
+        else {
+            startButton.GetComponent<Image>().color = Color.white;
+            startButton.transform.Find("Lock").gameObject.SetActive(false);
+        }
+
     }
 
     public void StartMission() {
