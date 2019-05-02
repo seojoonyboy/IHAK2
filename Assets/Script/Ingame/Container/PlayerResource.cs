6c12bfdf78b36f1abf8bfcc4a6481a244be3bf65
joonyboy <seojoonwon@naver.com>
2019-05-02 18:29:10 +0900

    게임 시작시 골드와 시민수 3만큼 갖고 시작하도록 수정.

--- Assets/Script/Ingame/Container/PlayerResource.cs
+++ Assets/Script/Ingame/Container/PlayerResource.cs
@@ -13,6 +13,12 @@ namespace Container {
         
         public int maxhp;
 
+        void Start() {
+            Debug.Log("시작 시민과 골드 보정");
+            Gold = 300;
+            Citizen = 300;
+        }
+
         public PlayerResource(int gold, int citizen) {
             Gold = gold;
             Citizen = citizen;
