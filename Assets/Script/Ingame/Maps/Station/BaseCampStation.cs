637e54ad6f8f0c699a964aef1c7be3d508120067
joonyboy <seojoonwon@naver.com>
2019-05-05 21:04:01 +0900

    전진기지 거점 점령 UI 추가

--- Assets/Script/Ingame/Maps/Station/BaseCampStation.cs
+++ Assets/Script/Ingame/Maps/Station/BaseCampStation.cs
@@ -1,3 +1,4 @@
+﻿using ingameUIModules;
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,10 +9,14 @@ public partial class BaseCampStation : DefaultStation {
 
     [SerializeField] [ReadOnly] protected bool startSeize = false;
     [SerializeField] [ReadOnly] protected bool rebuilding = false;
+    [SerializeField] [ReadOnly] GameObject occupySlider;
+    [SerializeField] [ReadOnly] CircularSlider circularSlider;
 
+    IEnumerator coroutine;
+    bool isAlreadyCoroutine = false;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         OwnerNum = PlayerController.Player.NEUTRAL;
         StationIdentity = StationBasic.StationState.BaseCamp;
         creepList = new List<GameObject>();
@@ -22,13 +27,16 @@ public partial class BaseCampStation : DefaultStation {
         towerComponent = tower.GetComponent<FowardHQ>();
         Invoke("SetMonsters", 3.0f);
         Invoke("SettingFog", 3.1f);
-        
+
+        occupySlider = Instantiate(Resources.Load("Prefabs/OccupySlider") as GameObject, transform);
+        circularSlider = occupySlider.GetComponent<CircularSlider>();
     }
 
 
     // Update is called once per frame
     void Update() {
         if (rebuilding) return;
+        //Debug.Log("creepList.Count" + creepList.Count + ", !startSeize" + !startSeize);
         if (towerComponent.IsDestroyed && creepList.Count == 0 && !startSeize) {
             startSeize = true;
             StartCoroutine(FindOwner());
@@ -45,27 +53,26 @@ public partial class BaseCampStation : DefaultStation {
 
     IEnumerator FindOwner() {
         int targetLayer = 0;
+        int time = 0;
         while (startSeize) {
-            foreach (GameObject target in targets.ToList()) {
-                if (target == null) continue;
-                if (targetLayer == 0) {
-                    targetLayer = target.layer;
-                    continue;
-                }
-                if ((int)OwnerNum != target.layer) {
-                    yield return new WaitForSeconds(0.1f);
-                    if (targetLayer != target.layer) startSeize = false;
+            //Debug.Log("loop");
+            if (canSeize()) {
+                time++;
+                circularSlider.ChangeByValue(time);
+                //Debug.Log(time);
+                if (time == 100) {
+                    targetLayer = targets[0].layer;
+                    OwnerNum = (PlayerController.Player)targetLayer;
+                    GetComponent<Collider2D>().enabled = false;
+                    targets.Clear();
+                    GetComponent<Collider2D>().enabled = true;
+                    StartCoroutine(RebuildTower());
+                    startSeize = false;
+                    if (OwnerNum == PlayerController.Player.PLAYER_1)
+                        IngameSceneEventHandler.Instance.PostNotification(IngameSceneEventHandler.MISSION_EVENT.NODE_CAPTURE_COMPLETE, this, null);
                 }
             }
             yield return new WaitForSeconds(0.1f);
-            OwnerNum = (PlayerController.Player)targetLayer;
-            GetComponent<Collider2D>().enabled = false;
-            targets.Clear();
-            GetComponent<Collider2D>().enabled = true;
-            StartCoroutine(RebuildTower());
-            startSeize = false;
-            if (OwnerNum == PlayerController.Player.PLAYER_1)
-                IngameSceneEventHandler.Instance.PostNotification(IngameSceneEventHandler.MISSION_EVENT.NODE_CAPTURE_COMPLETE, this, null);
         }
     }
 
@@ -81,7 +88,26 @@ public partial class BaseCampStation : DefaultStation {
         rebuilding = false;
     }
 
+    bool canSeize() {
+        var query =
+            from unit in targets
+            where (unit.GetComponent<HeroAI>() != null)
+            group unit by unit.GetComponent<UnitAI>().ownerNum;
 
+        int count = 0;
+        foreach (var group in query) {
+            count++;
+        }
+        //Debug.Log("그룹 : " + count);
+        if (count == 1) {
+            //Debug.Log("점령 진행");
+            return true;
+        }
+        else {
+            //Debug.Log("적이 감지되어 점령 중지");
+            return false;
+        }
+    }
 }
 
 public partial class BaseCampStation {
@@ -99,16 +125,16 @@ public partial class BaseCampStation {
         goblin.GetComponent<MonsterAI>().Init(AccountManager.Instance.neutralMonsterDatas.Find(x => x.id == "npc_monster_01001"));
         goblin.GetComponent<MonsterAI>().expPoint = 20;
 
-        foreach(Transform wayPoint in transform.GetChild(1)) {
+        foreach (Transform wayPoint in transform.GetChild(1)) {
             wayPoints.Add(wayPoint);
         }
 
         var creeps = AccountManager.Instance.mission.creeps;
-        foreach(DataModules.MonsterData monsterData in creeps) {
-            if(monsterData.creep.id == "npc_monster_01001") {
+        foreach (DataModules.MonsterData monsterData in creeps) {
+            if (monsterData.creep.id == "npc_monster_01001") {
                 int monsterCount = monsterData.count;
                 MissionConditionsController mc = GameObject.Find("Controllers").GetComponent<MissionConditionsController>();
-                if(mc.conditions != null) {
+                if (mc.conditions != null) {
                     ConditionSet expSet = PlayerController.Instance
                     .MissionConditionsController()
                     .conditions.Find(x => x.condition == Conditions.geojeom_monster_count);
@@ -119,7 +145,7 @@ public partial class BaseCampStation {
 
                 for (int i = 0; i < monsterCount; i++) {
                     GameObject generateMonster = Instantiate(goblin, monsterParent);
-                    generateMonster.transform.position = transform.GetChild(1).GetChild(i).position;
+                    generateMonster.transform.position = transform.GetChild(2).GetChild(i).position;
 
                     generateMonster.GetComponent<StateController>().SetupAI(true, wayPoints);
                     generateMonster.GetComponent<MonsterAI>().tower = this;
@@ -163,7 +189,7 @@ public partial class BaseCampStation : DefaultStation {
             collision.gameObject.AddComponent<RespawnMinion>();
         }
     }
-    
+
 
 
     void OnTriggerExit2D(Collider2D collision) {
@@ -171,7 +197,7 @@ public partial class BaseCampStation : DefaultStation {
         if ((collision.gameObject.layer != (int)OwnerNum) && collision.GetComponent<UnitAI>() != null) {
             targets.Remove(collision.gameObject);
 
-            if(towerComponent.Enemy = collision.transform) {
+            if (towerComponent.Enemy = collision.transform) {
                 if (targets.Count <= 0) towerComponent.Enemy = null;
                 else towerComponent.Enemy = targets[0].transform;
             }
@@ -180,5 +206,5 @@ public partial class BaseCampStation : DefaultStation {
         if (collision.GetComponent<UnitGroup>() != null && (collision.transform.GetChild(2).gameObject.layer == (int)OwnerNum)) {
             Destroy(collision.gameObject.GetComponent<RespawnMinion>());
         }
-    }    
+    }
 }
